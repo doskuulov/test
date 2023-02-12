8a321a090935305717e44af8472cd2ec1b863280
Doskulov Mirbek <iskender.doskulov@gmail.com>
2023-02-12 14:54:37 +0600

    add api

--- index.js
+++ index.js
@@ -15,3 +15,34 @@ window.addEventListener('scroll', function () {
 	let header = document.querySelector('header')
 	header.classList.toggle('sticky', window.scrollY > 50)
 })
+//
+
+async function getResponse() {
+	let response = await fetch('https://63cb6c96f36cbbdfc769b8aa.mockapi.io/key')
+	let content = await response.json()
+	content = content.splice(0, 12)
+
+	let list = document.querySelector('.box')
+	let key
+
+	for (key in content) {
+		list.innerHTML += `<div class="box">
+						<div class="image">
+							<div class="img">
+								<img class="wrap" src="${content[key].avatar}" alt="" />
+							</div>
+							<div class="overlay">
+								<i class="far fa-search"></i>
+								<i class="far fa-user"></i>
+								<i class="far fa-heart"></i>
+							</div>
+						</div>
+						<div class="text">
+							<h4>${content[key].name}</h4>
+							<h4><span>$${content[key].discount}</span> $${content[key].number}</h4>
+						</div>
+					</div>`
+	}
+}
+
+getResponse()
